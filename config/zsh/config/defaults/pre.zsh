9b5f9b383a0cb88b17052ee25a28562e447f884a
Iivari Leinonen <illein.dev@gmail.com>
2025-11-20 14:38:51 +0200

    update colors

--- config/zsh/config/defaults/pre.zsh
+++ config/zsh/config/defaults/pre.zsh
@@ -1,4 +1,15 @@
-# Add empty line between prompt and output
+# Set eza theme based on system appearance
+update_eza_theme() {
+  dark_mode=$(osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode')
+  if [[ "$dark_mode" == "true" ]]; then
+    theme="catppuccin-frappe-lavender"
+  else
+    theme="catppuccin-latte-sky"
+  fi
+  ln -sf "$HOME/.config/eza/themes/$theme.yml" "$HOME/.config/eza/theme.yml"
+}
+
 preexec() {
-  echo
+  update_eza_theme
+  echo # Add a newline before output
 }
