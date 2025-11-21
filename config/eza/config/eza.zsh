18e87371ae45e332189a6fa397f78caaebd105f9
Iivari Leinonen <illein.dev@gmail.com>
2025-11-21 06:11:41 +0200

    eza options

--- config/eza/config/eza.zsh
+++ config/eza/config/eza.zsh
@@ -2,5 +2,5 @@
 export EZA_CONFIG_DIR="$HOME/.config/eza"
 
 # Eza aliases (modern ls replacement)
-alias ls="eza -l --icons"    # List view with icons
-alias lst="eza -T --icons"   # Tree view with icons
+alias ls="eza --long --icons --no-user" # List view
+alias lst="eza --tree --icons --no-user" # Tree view
