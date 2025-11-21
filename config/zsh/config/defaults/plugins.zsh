3033cdd9701fcb805f887b29ddf0a21a887f12a3
Iivari Leinonen <illein.dev@gmail.com>
2025-11-21 06:07:23 +0200

    cleanup zsh plugins

--- config/zsh/config/defaults/plugins.zsh
+++ config/zsh/config/defaults/plugins.zsh
@@ -5,7 +5,6 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 source "${ZINIT_HOME}/zinit.zsh"
 
 # Load plugins
-zinit light Aloxaf/fzf-tab
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
 zinit light zsh-users/zsh-syntax-highlighting
@@ -18,35 +17,5 @@ zinit snippet OMZP::git
 autoload -U colors && colors
 autoload -U compinit && compinit
 
-# Replay cached completions
-zinit cdreplay -q
-
-# Keybindings
-bindkey -e                          # Emacs mode
-bindkey '^p' history-search-backward
-bindkey '^n' history-search-forward
-
-# History configuration
-HISTDUP=erase
-HISTFILE=~/.zsh_history
-HISTSIZE=5000
-SAVEHIST=$HISTSIZE
-
-setopt appendhistory
-setopt hist_find_no_dups
-setopt hist_ignore_all_dups
-setopt hist_ignore_dups
-setopt hist_ignore_space
-setopt hist_save_no_dups
-setopt sharehistory
-
-# Completion styles
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
-
 # Shell integrations
-eval "$(fzf --zsh)"
 eval "$(zoxide init --cmd cd zsh)"
